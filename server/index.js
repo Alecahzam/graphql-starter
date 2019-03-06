e7a2c8346c0421d91a8c8a62a80c2450157ac1ec
Jonathan May <jonathan.may@devmountain.com>
2019-03-05 22:03:22 -0600

    Add basic server template

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
+const { readFileSync } = require("fs");
+const { ApolloServer } = require("apollo-server");
 
+const typeDefs = readFileSync(`${__dirname}/schema/typeDefs.graphql`, "utf8");
+const resolvers = require("./schema/resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ctx => ({
+    ...ctx.request
+  })
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
